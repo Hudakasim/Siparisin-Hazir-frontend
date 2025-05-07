97147a0a09117dde60d7a7dbf8a13c8e27a3beee
Fatma Cicek <fatmacicek139@gmail.com>
2025-05-07 07:44:34 +0300

    fatma

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <section className="about" id="about">
       <div className="about-heading">
         <h2>Biz Kimiz</h2>
-        <p>Siparişin Hazır ekibi olarak, öğrencilerin kampüs hayatını kolaylaştırmayı amaçlayan yenilikçi çözümler geliştiriyoruz...</p>
+        <p>Siparişin Hazır ekibi olarak, öğrencilerin kampüs hayatını kolaylaştırmayı amaçlayan yenilikçi çözümler geliştiriyoruz</p>
       </div>
 
       <div className="about-content">
